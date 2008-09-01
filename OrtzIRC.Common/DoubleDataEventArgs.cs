ccaa10795c8d52e346f71c14752846fcc0683b1c
schmeling88 <schmeling88@gmail.com>
2008-09-01 18:05:37 +0000

    Added some common EventArgs classes and an EventUtil.

---
+++ OrtzIRC.Common/DoubleDataEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace OrtzIRC.Common
+{
+    using System;
+
+    public class DoubleDataEventArgs<TFirst, TSecond> : EventArgs
+    {
+        public TFirst First { get; private set; }
+
+        public TSecond Second { get; private set; }
+
+        public DoubleDataEventArgs(TFirst first, TSecond second)
+        {
+            this.First = first;
+            this.Second = second;
+        }
+    }
+}
