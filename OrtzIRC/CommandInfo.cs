203b2b85cbcfba9550d89c6c92ffc66b45d07c2a
Ortzinator <brian@ortzirc.com>
2008-10-21 00:34:02 +0000

    * Removed PluginInfo and renamed Command.cs to CommandInfo.cs * Fixed more coding dumbery

---
+++ OrtzIRC/CommandInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace OrtzIRC.Plugins
+{
+    internal class CommandInfo
+    {
+        public string AssemblyPath { get; set; }
+        public string ClassName { get; set; }
+
+        public CommandInfo(string path, string name)
+        {
+            this.AssemblyPath = path;
+            this.ClassName = name;
+        }
+    }
+}
