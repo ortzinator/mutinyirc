0f6c4eccabf52a2e1231c4f6a956cdd113d05d30
Ortzinator <brian@ortzirc.com>
2008-12-12 01:05:13 +0000

    basic structure for addins

---
+++ OrtzIRC.HostSideAdapters/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrtzIRC.HostSideAdapters
+{
+    public class Class1
+    {
+    }
+}
---
+++ OrtzIRC.HostViews/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrtzIRC.HostViews
+{
+    public class Class1
+    {
+    }
+}
