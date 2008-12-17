4b3d01dfd201fda8a7917f9f8fbc998671338970
Ortzinator <brian@ortzirc.com>
2008-12-17 03:16:12 +0000

    Fixed some stuff I broke in the solution

---
+++ FlamingIRC/Events/ErrorMessageEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace FlamingIRC
+{
+    using System;
+
+    /// <summary>
+    /// Error messages from the IRC server.
+    /// </summary>
+    public class ErrorMessageEventArgs : EventArgs
+    {
+        public ReplyCode Code;
+        public string Message;
+
+        public ErrorMessageEventArgs(ReplyCode code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+    }
+}
