73a09e617b03ee7377a3b08280e1fba2d8f2280e
Ortzinator <brian@ortzirc.com>
2008-12-17 03:16:12 +0000

    Fixed some stuff I broke in the solution

---
+++ FlamingIRC/DoubleDataEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace FlamingIRC
+{
+    using System;
+
+    public class FlamingDoubleDataEventArgs<TFirst, TSecond> : EventArgs
+    {
+        public TFirst First { get; private set; }
+
+        public TSecond Second { get; private set; }
+
+        public FlamingDoubleDataEventArgs(TFirst first, TSecond second)
+        {
+            this.First = first;
+            this.Second = second;
+        }
+    }
+}
---
+++ FlamingIRC/EventUtil.cs
@@ -0,0 +1,23 @@
+﻿namespace FlamingIRC
+{
+    using System;
+
+    internal static class EventUtil
+    {
+        public static void Fire<TEventArgs>(this EventHandler<TEventArgs> myEvent, object sender, TEventArgs e) where TEventArgs : EventArgs
+        {
+            if (myEvent != null)
+            {
+                myEvent(sender, e);
+            }
+        }
+
+        public static void Fire<TEventArgs>(this EventHandler myEvent, object sender, TEventArgs e) where TEventArgs : EventArgs
+        {
+            if (myEvent != null)
+            {
+                myEvent(sender, e);
+            }
+        }
+    }
+}
