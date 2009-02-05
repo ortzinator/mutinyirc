176457359be47bdbba6e8a7edd5f40896c88dc6a
Ortzinator <brian@ortzirc.com>
2009-02-05 02:30:10 +0000

    *Window manager nodes are now removed when their respective windows close *Channel windows now close when the server window is closed.

--- OrtzIRC/Controls/ServerTreeNode.cs
+++ OrtzIRC/Controls/ServerTreeNode.cs
@@ -10,6 +10,12 @@
         {
             this.ServerWindow = serverWindow;
             this.Text = serverWindow.Server.Description;
+            serverWindow.FormClosed += new FormClosedEventHandler(serverWindow_FormClosed);
+        }
+
+        private void serverWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Remove();
         }
 
         public void AddChannelNode(ChannelTreeNode channelNode)
