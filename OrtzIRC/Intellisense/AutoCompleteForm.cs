d440ff3099f29328fb2ffd97e5ef593f1a44608c
Max <max@ortzirc.com>
2009-02-05 04:14:52 +0000

    Added auto complete form. Very humble beginnings.

---
+++ OrtzIRC/Intellisense/AutoCompleteForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OrtzIRC.Intellisense
+{
+    public partial class AutoCompleteForm : Form
+    {
+        public AutoCompleteForm(Point position)
+        {
+            InitializeComponent();
+
+            this.Top = position.Y + 5;
+            this.Left = position.X + 5;
+        }
+    }
+}
