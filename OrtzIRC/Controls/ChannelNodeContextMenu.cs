90bfb5b003dc9edcbce617da388e7b8c3de44ec5
Ortzinator <brian@ortzirc.com>
2009-03-01 20:51:40 +0000

    Added some context menus to the window treeview.

---
+++ OrtzIRC/Controls/ChannelNodeContextMenu.cs
@@ -0,0 +1,15 @@
+﻿namespace OrtzIRC
+{
+    using System;
+    using System.Windows.Forms;
+
+    public class ChannelNodeContextMenu : ContextMenuStrip
+    {
+        public event EventHandler LeaveClick;
+
+        public ChannelNodeContextMenu()
+        {
+            Items.Add("Leave").Click += delegate { LeaveClick.Fire(this, new EventArgs()); };
+        }
+    }
+}
