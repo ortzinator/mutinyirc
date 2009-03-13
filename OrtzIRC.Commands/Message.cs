51def8207c1b820552a230fb5056eff61ba25023
Max <max@ortzirc.com>
2009-03-13 03:31:35 +0000

    Added Message command

---
+++ OrtzIRC.Commands/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace OrtzIRC.Commands
+{
+    using Common;
+    using PluginFramework;
+
+    /// <summary>
+    /// A command that sends private messages.
+    /// </summary>
+    [Plugin("Msg")]
+    public class Message
+    {
+        /// <summary>
+        /// Sends a private message to the specified user.
+        /// </summary>
+        /// <param name="context">The context where the command is executed from.</param>
+        /// <param name="user">The user to send the message to.</param>
+        /// <param name="message">The message to send.</param>
+        public void Execute(MessageContext context, UserInfo user, string message)
+        {
+            // todo - implement message command; MessageContext needs to have a Send method (it can be abstract if necessary)
+        }
+    }
+}
