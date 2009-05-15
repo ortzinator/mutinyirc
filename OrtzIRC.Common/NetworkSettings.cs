0423a731d9380ef4245ee77450135fd75f379312
Ortzinator <brian@ortzirc.com>
2009-05-15 02:27:38 +0000

    starting on server settings with sqlite

---
+++ OrtzIRC.Common/NetworkSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace OrtzIRC.Common
+{
+    public struct NetworkSettings
+    {
+        public string Name { get; private set; }
+    }
+}
---
+++ OrtzIRC.Common/ServerSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace OrtzIRC.Common
+{
+    public class ServerSettings
+    {
+        public ServerSettings(string uri, string description, int port, bool ssl)
+        {
+            Uri = uri;
+            Description = description;
+            Port = port;
+            Ssl = ssl;
+        }
+
+        public string Uri { get; private set; }
+        public string Description { get; private set; }
+        public int Port { get; private set; }
+        public bool Ssl { get; private set; }
+    }
+}
