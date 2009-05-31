0245d29c1741eb961104fb0ab01e3eda6211dade
Ortzinator <brian@ortzirc.com>
2009-05-31 23:09:47 +0000

    Basic SQLite db functionality working

--- OrtzIRC.Common/NetworkSettings.cs
+++ OrtzIRC.Common/NetworkSettings.cs
@@ -3,5 +3,10 @@
     public class NetworkSettings
     {
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
