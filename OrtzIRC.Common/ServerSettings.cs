0e8c1c1838c78c44402fa8228b672b31b63e9545
gparent <gparent+ortzhg@gmail.com>
2009-06-21 01:02:16 +0000

    -Replaced function stub by appropriate string-to-int[] function.

--- OrtzIRC.Common/ServerSettings.cs
+++ OrtzIRC.Common/ServerSettings.cs
@@ -1,4 +1,7 @@
-﻿namespace OrtzIRC.Common
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrtzIRC.Common
 {
     public class ServerSettings
     {
@@ -26,12 +29,63 @@
             }
         }
 
-
-        private int[] PortsStringToArray(string ports)
+        private static int[] PortsStringToArray(string ports)
         {
-            var bits = ports.Split(',');
+            string[] portListChunk = ports.Split(',');
 
-            return new int[] {}; //TODO
+            List<int> portList = new List<int>(portListChunk.Length); // Maximum performance if there are no ranges
+
+            foreach (string chunk in portListChunk)
+            {
+                if (chunk.Contains("-"))
+                {
+                    // Chunk is a range
+                    string[] rangeParts = chunk.Split('-');
+
+                    // User entered something stupid like "30-;"
+                    if (rangeParts.Length != 2)
+                        throw new FormatException();
+
+                    int beginRange;
+                    int endRange;
+
+                    try
+                    {
+                        beginRange = Int32.Parse(rangeParts[0]);
+                        endRange = Int32.Parse(rangeParts[1]);
+                    }
+                    catch (Exception)
+                    {
+                        throw new FormatException();
+                    }
+
+                    // User entered something dumb like "30-20" or "20-20"
+                    if (endRange <= beginRange)
+                        throw new FormatException();
+
+                    // Add entire range to the list
+                    while (beginRange <= endRange && !portList.Contains(beginRange))
+                        portList.Add(beginRange++);
+                }
+                else // Chunk is a single port number
+                {
+                    int tempNum;
+
+                    try
+                    {
+                        tempNum = Int32.Parse( chunk );
+                    }
+                    catch (Exception)
+                    {
+                        throw new FormatException();
+                    }
+
+                    if (!portList.Contains(tempNum))
+                        portList.Add(tempNum);
+                }
+            }
+
+            return portList.ToArray();
         }
     }
 }
