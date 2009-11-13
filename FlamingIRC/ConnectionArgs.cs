b619277942552a599ad4e72795817d619d383185
Ortzinator <brian@ortzirc.com>
2009-11-12 21:39:23 -0500

    Rafactored FlamingIRC.Connection and implimented blankthemuffin's TcpTextClient, code cleanup.

--- FlamingIRC/ConnectionArgs.cs
+++ FlamingIRC/ConnectionArgs.cs
@@ -30,14 +30,6 @@ namespace FlamingIRC
     /// </summary>
     public struct ConnectionArgs
     {
-        private string realName;
-        private string nickName;
-        private string userName;
-        private string modeMask;
-        private string hostname;
-        private int port;
-        private string serverPassword;
-
         /// <summary>
         /// Create a new instance initialized with the default values:
         /// TCP/IP port 6667, no server password, and user mode
@@ -46,122 +38,63 @@ namespace FlamingIRC
         /// <param name="name">The nick, user name, and real name are 
         /// all set to this value.</param>
         /// <param name="hostname">The hostname of the IRC server.</param>
-        public ConnectionArgs(string name, string hostname)
+        /// <param name="ssl">Whether to enable SSL</param>
+        public ConnectionArgs(string name, string hostname, bool ssl)
+            : this()
         {
-            realName = name;
-            nickName = name;
-            userName = name.ToLower();
-            modeMask = "4";
-            this.hostname = hostname;
-            port = 6667;
-            serverPassword = "*";
+            RealName = name;
+            Ssl = ssl;
+            Nick = name;
+            UserName = name.ToLower();
+            ModeMask = "4";
+            Hostname = hostname;
+            Port = 6667;
+            ServerPassword = "*";
         }
 
         /// <summary>
         /// The IRC server hostname
         /// </summary>
         /// <value>The full hostname such as irc.gamesnet.net</value>
-        public string Hostname
-        {
-            get
-            {
-                return hostname;
-            }
-            set
-            {
-                hostname = value;
-            }
-        }
+        public string Hostname { get; set; }
+
         /// <summary>
         /// Set's the user's initial IRC mode mask. Set to 0 to recieve wallops
         /// and be invisible. Set to 4 to be invisible and not receive wallops.
         /// </summary>
         /// <value>A number mask such as 0 or 4.</value>
-        public string ModeMask
-        {
-            get
-            {
-                return modeMask;
-            }
-            set
-            {
-                modeMask = value;
-            }
-        }
+        public string ModeMask { get; set; }
+
         /// <summary>
         /// The user's nick name.
         /// </summary>
         /// <value>A string which conforms to the IRC nick standard.</value>
-        public string Nick
-        {
-            get
-            {
-                return nickName;
-            }
-            set
-            {
-                nickName = value;
-            }
-        }
+        public string Nick { get; set; }
+
         /// <summary>
         /// The TCP/IP port the IRC listens server listens on.
         /// </summary>
         /// <value> Normally should be set to 6667. </value>
-        public int Port
-        {
-            get
-            {
-                return port;
-            }
-            set
-            {
-                port = value;
-            }
-        }
+        public int Port { get; set; }
+
         /// <summary>
         /// The user's 'real' name.
         /// </summary>
         /// <value>A short string with any legal characters.</value>
-        public string RealName
-        {
-            get
-            {
-                return realName;
-            }
-            set
-            {
-                realName = value;
-            }
-        }
+        public string RealName { get; set; }
+
         /// <summary>
         /// The user's machine logon name.
         /// </summary>
         /// <value>A short string with any legal characters.</value>
-        public string UserName
-        {
-            get
-            {
-                return userName;
-            }
-            set
-            {
-                userName = value;
-            }
-        }
+        public string UserName { get; set; }
+
         /// <summary>
         /// The password for this server. These are seldomly used. Set to '*' 
         /// </summary>
         /// <value>A short string with any legal characters.</value>
-        public string ServerPassword
-        {
-            get
-            {
-                return serverPassword;
-            }
-            set
-            {
-                serverPassword = value;
-            }
-        }
+        public string ServerPassword { get; set; }
+
+        public bool Ssl { get; set; }
     }
 }
