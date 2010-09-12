76ca5f3d231efed78d0155ffe1051dad9550f7ae
Ortzinator <brian@ortzirc.com>
2010-09-12 14:57:14 -0400

    Abstracted chat rows to allow for different styles

---
+++ WPFFrontend/ChatItem.cs
@@ -0,0 +1,28 @@
+﻿namespace WPFFrontend
+{
+    using System;
+    using FlamingIRC;
+
+    public class ChatItem
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+
+        public ChatItem(DateTime time, string message)
+        {
+            Time = time;
+            Message = message;
+        }
+    }
+
+    public class ChannelMessageChatItem : ChatItem
+    {
+        public User User { get; set; }
+
+        public ChannelMessageChatItem(DateTime time, string message, User user)
+            : base(time, message)
+        {
+            User = user;
+        }
+    }
+}
--- WPFFrontend/MainWindow.xaml.cs
+++ WPFFrontend/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows.Input;
+using FlamingIRC;
 
 namespace WPFFrontend
 {
@@ -17,7 +19,16 @@ namespace WPFFrontend
 
         private void ChannelPanel_CommandEntered(object sender, CommandEventArgs e)
         {
-            ((ChatCollection)Resources["chatCollection"]).Add(new ChatRowItem(){Time = DateTime.Now, User = "Ortzinator", Message = e.Data});
+            ((ChatCollection)Resources["chatCollection"]).Add(new ChannelMessageChatItem { Time = DateTime.Now, User = new User { Nick = "Ortzinator" }, Message = e.Data });
+        }
+
+        protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.T)
+            {
+                ((ChatCollection)Resources["chatCollection"]).Add(new ChatItem{});
+            }
+            base.OnKeyDown(e);
         }
     }
 }
