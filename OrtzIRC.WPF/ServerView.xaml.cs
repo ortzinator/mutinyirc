4f59b864ce31191f012bed8a3a5e1fb10cbfcb4b
Ortzinator <brian@ortzirc.com>
2010-09-21 00:09:14 -0400

    Far-too-large commit adding lots of stuff I cba to list.

---
+++ OrtzIRC.WPF/ServerView.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace OrtzIRC.WPF
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ServerView.xaml
+    /// </summary>
+    public partial class ServerView : UserControl
+    {
+        public ServerView()
+        {
+            InitializeComponent();
+        }
+
+        private void commandBox_CommandEntered(object sender, CommandEventArgs e)
+        {
+            outputBox.scrollViewer.ScrollToBottom();
+        }
+    }
+}
---
+++ OrtzIRC.WPF/ViewModels/ServerViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace OrtzIRC.WPF.ViewModels
+{
+    using System;
+    using FlamingIRC;
+    using OrtzIRC.Common;
+    using System.Windows.Threading;
+
+    public class ServerViewModel : IrcViewModel
+    {
+        public Server Server { get; private set; }
+
+        public ServerViewModel(Server server)
+        {
+            Server = server;
+            Name = Server.Url;
+            Server.RawMessageReceived += Server_RawMessageReceived;
+        }
+
+        private void Server_RawMessageReceived(object sender, DataEventArgs<string> e)
+        {
+            ChatLines.Add(new ChatItemViewModel(DateTime.Now, e.Data));
+        }
+    }
+}
