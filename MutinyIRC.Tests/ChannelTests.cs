ad322ec8e8afd54a6ccccd3d9591e73646fae635
Brian Ortiz <ortzinator@gmail.com>
2013-10-22 17:52:56 -0400

    Changed test project name and added a few tests.

---
+++ MutinyIRC.Tests/ChannelTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OrtzIRC.Common;
+using FakeItEasy;
+
+namespace MutinyIRC.Tests
+{
+    [TestFixture]
+    public class ChannelTests
+    {
+        private Channel _channel;
+
+        [SetUp]
+        public void Setup()
+        {
+            Server serverMock = A.Fake<Server>();
+            _channel = new Channel(serverMock, "#mutiny");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _channel = null;
+        }
+
+        [Test]
+        public void ShowTopic_TopicRecievedRegistered_EventFires()
+        {
+            bool eventWasRaised = false;
+            _channel.TopicReceived += (sender, args) => eventWasRaised = true;
+            _channel.ShowTopic("Topic here and stuff");
+            Assert.IsTrue(eventWasRaised, "TopicRecieved event was not fired");
+        }
+    }
+}
---
+++ MutinyIRC.Tests/UserListTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using FlamingIRC;
+using NUnit.Framework;
+
+namespace MutinyIRC.Tests
+{
+    /// <summary>
+    ///This is a test class for UserListTest and is intended
+    ///to contain all UserListTest Unit Tests
+    ///</summary>
+    [TestFixture]
+    public class UserListTests
+    {
+        /// <summary>
+        ///A test for Sort
+        ///</summary>
+        [Test]
+        public void SortTest()
+        {
+            UserList target = new UserList();
+            target.Add(new User { Nick = "Ortzinator"});
+            target.Add(new User { Nick = "P90" });
+            target.Add(new User { Nick = "gparent" });
+            target.Add(new User { Nick = "DEADBEEF" });
+            target.Add(new User { Nick = "@ChanServ" });
+            target.Add(new User { Nick = "+SteveUK" });
+            target.Add(new User { Nick = "@SpamServ" });
+            target.Add(new User { Nick = "+HTF" });
+            target.Add(new User { Nick = "+ninjers" });
+            Comparison<User> comparison = (user1, user2) => user1.CompareTo(user2);
+            target.Sort(comparison);
+            foreach (User user in target)
+            {
+                Debug.WriteLine(user.Nick);
+            }
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+    }
+}
