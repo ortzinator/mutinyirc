5758b8453efa88ef53fdcf11648b4ec7641f1ff3
Brian Ortiz <ortzinator@gmail.com>
2013-10-22 17:52:56 -0400

    Changed test project name and added a few tests.

---
+++ MutinyIRC.Tests/ServerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FlamingIRC;
+using NUnit.Framework;
+using OrtzIRC.Common;
+
+namespace MutinyIRC.Tests
+{
+    [TestFixture]
+    public class ServerTests
+    {
+        private Server _server;
+
+        [SetUp]
+        public void Setup()
+        {
+            _server = new Server();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _server = null;
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetupConnection_NickNotSet_ArgumentExceptionThrown()
+        {
+            _server.SetupConnection(new ConnectionArgs());
+        }
+    }
+}
