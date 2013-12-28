4b97f76cbc92bd45c65bce37b5c2f1169a3bc8b1
Brian Ortiz <ortzinator@gmail.com>
2013-12-28 01:27:18 -0500

    Refactored User more

--- FlamingIRC/User.cs
+++ FlamingIRC/User.cs
@@ -31,43 +31,15 @@ namespace FlamingIRC
     /// </summary>
     public class User : IComparable<User>
     {
+        private string _hostMask = String.Empty;
+
         private string _nick = String.Empty;
-        /// <summary>The user's nickname.</summary>
-        public string Nick { get; set; }
 
-        /// <summary>The user's "real name", immediately before the @</summary>
-        public string RealName { get; set; }
+        private char _prefix;
 
-        /// <summary>The user's fully qualified host name</summary>
-        public string HostMask { get; set; }
+        private string _realName = String.Empty;
 
-        /// <summary>The user's username on the local machine</summary>
-        public string UserName { get; set; }
-
-        /// <summary> Nickname plus mode symbol prefix </summary>
-        public string NamesLiteral { get { return Prefix != '\0' ? Prefix + Nick : Nick; } }
-
-        /// <summary> The channel mode symbol prefix from NAMES</summary>
-        public char Prefix
-        {
-            get
-            {
-                return _prefix;
-            }
-            set
-            {
-                if (value != '\0' && !UserModeValidator.IsValid(value))
-                {
-                    throw new ArgumentOutOfRangeException();
-                }
-                _prefix = value;
-            }
-        }
-
-        public static User Empty
-        {
-            get { return new User(); }
-        }
+        private string _userName = String.Empty;
 
         public User() { }
 
@@ -86,8 +58,57 @@ namespace FlamingIRC
             Prefix = '\0';
         }
 
-        private char _prefix;
+        public static User Empty
+        {
+            get { return new User(); }
+        }
 
+        /// <summary>The user's fully qualified host name</summary>
+        public string HostMask
+        {
+            get { return _hostMask; }
+            set { _hostMask = value; }
+        }
+
+        /// <summary> Nickname plus mode symbol prefix </summary>
+        public string NamesLiteral { get { return Prefix != '\0' ? Prefix + Nick : Nick; } }
+
+        /// <summary>The user's nickname.</summary>
+        public string Nick
+        {
+            get { return _nick; }
+            set { _nick = value; }
+        }
+
+        /// <summary> The channel mode symbol prefix from NAMES</summary>
+        public char Prefix
+        {
+            get
+            {
+                return _prefix;
+            }
+            set
+            {
+                if (value != '\0' && !UserModeValidator.IsValid(value))
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _prefix = value;
+            }
+        }
+
+        /// <summary>The user's "real name", immediately before the @</summary>
+        public string RealName
+        {
+            get { return _realName; }
+            set { _realName = value; }
+        }
+        /// <summary>The user's username on the local machine</summary>
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
         /// <summary>
         ///   Takes a nick string from a NAMES and parses it as a User object
         /// </summary>
@@ -112,6 +133,11 @@ namespace FlamingIRC
             return user;
         }
 
+        public int CompareTo(User other)
+        {
+            return NamesLiteral.CompareTo(other.NamesLiteral);
+        }
+
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
@@ -133,12 +159,6 @@ namespace FlamingIRC
         {
             return (Nick != null ? Nick.GetHashCode() : 0);
         }
-
-        public int CompareTo(User other)
-        {
-            return NamesLiteral.CompareTo(other.NamesLiteral);
-        }
-
         public override string ToString()
         {
             return NamesLiteral;
