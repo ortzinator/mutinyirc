04239f876dd2da8f6124ea523f59e616754449c5
Brian Ortiz <ortzinator@gmail.com>
2014-04-25 16:57:02 -0400

    Writing a new parser, with tests!

---
+++ FlamingIRC.Tests/ListenerTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FakeItEasy;
+
+namespace FlamingIRC.Tests
+{
+    [TestFixture]
+    public class ListenerTests
+    {
+        private Listener _listener;
+        private const string _pong = ":hitchcock.freenode.net PONG hitchcock.freenode.net :ping";
+        private const string _names = ":hitchcock.freenode.net 353 OrtzIRC = #ortzirc :Ortzinator OrtzIRC @ChanServ";
+        private const string _namesEnd = ":hitchcock.freenode.net 366 OrtzIRC #ortzirc :End of /NAMES list.";
+        private const string _quit = ":Ortzinator!~ortz@cpe-075-184-002-005.nc.res.rr.com QUIT :Quit: Leaving";
+        private const string _join = ":Ortzinator!~ortz@cpe-075-184-002-005.nc.res.rr.com JOIN #ortzirc";
+        private const string _notice = ":hitchcock.freenode.net NOTICE * :*** Looking up your hostname...";
+
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+        }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            _listener = new Listener();
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            _listener = null;
+        }
+
+        [Test]
+        public void ParseIrcMessage_PongMessage()
+        {
+            IrcMessage msg = _listener.ParseIrcMessage(_pong);
+            Assert.AreEqual("PONG", msg.Command);
+            Assert.AreEqual("hitchcock.freenode.net", msg.From);
+        }
+
+        [Test]
+        public void ParseIrcMessage_NoticeMessage()
+        {
+            IrcMessage msg = _listener.ParseIrcMessage(_notice);
+            Assert.AreEqual("hitchcock.freenode.net", msg.From);
+            Assert.AreEqual("NOTICE", msg.Command);
+            Assert.AreEqual("*** Looking up your hostname...", msg.Message);
+        }
+
+        [Test]
+        public void ParseIrcMessage_NamesMessage()
+        {
+            IrcMessage msg = _listener.ParseIrcMessage(_names);
+            Assert.AreEqual("hitchcock.freenode.net", msg.From);
+            Assert.AreEqual(ReplyCode.RPL_NAMREPLY, msg.ReplyCode);
+            Assert.AreEqual("Ortzinator OrtzIRC @ChanServ", msg.Message);
+        }
+
+        [Test]
+        public void RemoveLeadingColon()
+        {
+            Assert.AreEqual("foobar", _listener.RemoveLeadingColon(":foobar"));
+            Assert.AreEqual("foobar", _listener.RemoveLeadingColon("foobar"));
+        }
+    }
+}
---
+++ FlamingIRC/IrcMessage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlamingIRC
+{
+    public class IrcMessage
+    {
+        private string _command;
+        private string _from;
+        private string _message;
+        private ReplyCode _replyCode;
+
+        public string Command
+        {
+            get { return _command; }
+            set { _command = value; }
+        }
+
+        public string From
+        {
+            get { return _from; }
+            set { _from = value; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        public ReplyCode ReplyCode
+        {
+            get { return _replyCode; }
+            set { _replyCode = value; }
+        }
+    }
+}
