9aa1fc6a463156e224beccfa45647853d159ea0f
Brian Ortiz <ortzinator@gmail.com>
2014-04-28 23:59:32 -0400

    Renamed InviteSentEventArgs to InviteEventArgs Refactored join and invite command processing

---
+++ FlamingIRC/Events/InviteEventArgs.cs
@@ -0,0 +1,29 @@
+﻿namespace FlamingIRC
+{
+    using System;
+
+    public class InviteEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The nick of the user who was invited
+        /// </summary>
+        public string Nick;
+
+        /// <summary>
+        /// The name of the channel the user was invited to join
+        /// </summary>
+        public string Channel;
+
+        /// <summary>
+        /// An invite was either sent or recieved
+        /// </summary>
+        /// <param name="nick">The nick of the user who was invited or sent the invite</param>
+        /// <param name="channel">The name of the channel the user was invited to join</param>
+        /// <seealso cref="Listener.OnInviteSent"/>
+        public InviteEventArgs(string nick, string channel)
+        {
+            Nick = nick;
+            Channel = channel;
+        }
+    }
+}
