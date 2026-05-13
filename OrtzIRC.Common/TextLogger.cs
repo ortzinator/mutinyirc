63d92ce6f7015293f9acdd8f1fbeec5d0706da10
Ortzinator <ortzinator@gmail.com>
2026-05-12 22:01:47 -0400

    [MutinyIRC] Convert TextLogger comments to XML docs and default TimeFormat to HH:mm:ss

--- OrtzIRC.Common/TextLogger.cs
+++ OrtzIRC.Common/TextLogger.cs
@@ -1,98 +1,90 @@
-﻿namespace OrtzIRC.Common
+using System;
+using System.Collections.Generic;
+using FlamingIRC;
+
+namespace OrtzIRC.Common
 {
-    using System;
-    using System.Collections.Generic;
-
-    using FlamingIRC;
-
     /// <summary>
-    /// Purpose: Log text entry by registering to various channel and network/pmsg events
-    ///          and writing their output to a text file.       
+    ///     Writes IRC text events (server, channel, private message) to log files.
     /// </summary>
     public static class TextLogger
     {
-        // Indicate any IO errors that can't be fixed
+        /// <summary>Whether to prepend a timestamp to each log entry.</summary>
+        public static bool AddTimestamp;
+
+        /// <summary>
+        ///     Format string passed to <see cref="DateTime.ToString(string)" /> when <see cref="AddTimestamp" /> is
+        ///     <c>true</c>.
+        /// </summary>
+        public static string TimeFormat = "HH:mm:ss";
+
+        private static readonly Dictionary<string, Dictionary<string, LoggedItem>> LogFiles = new();
+
+        /// <summary>Raised when a write operation fails due to an I/O error that cannot be recovered.</summary>
         public static event EventHandler<DataEventArgs<string>> WriteFailed;
 
-        // Whether we should add time or not
-        public static bool addTimestamp;
-
-        // Timestamp
-        public static String timeFormat;
-
-        // Main datastructure
-        private static Dictionary<string, Dictionary<string, LoggedItem>> LogFiles =
-            new Dictionary<string, Dictionary<String, LoggedItem>>();
-
-        // Logging functions
-
-        public static void TextEntry(Server network, String text)
+        /// <summary>Logs a server-level text entry.</summary>
+        public static void TextEntry(Server network, string text)
         {
             WriteText(LogFiles[network.Url]['!' + network.Url], text);
         }
 
-        public static void TextEntry(Server network, User person, String text)
+        /// <summary>Logs a private-message text entry.</summary>
+        public static void TextEntry(Server network, User person, string text)
         {
             WriteText(LogFiles[network.Url][person.Nick], text);
         }
 
-        public static void TextEntry(Channel chan, String text)
+        /// <summary>Logs a channel text entry.</summary>
+        public static void TextEntry(Channel chan, string text)
         {
             WriteText(LogFiles[chan.Server.Url][chan.Name], text);
         }
 
-        private static void WriteText(LoggedItem logger, String text)
+        private static void WriteText(LoggedItem logger, string text)
         {
-            logger.Write(addTimestamp ? DateTime.Now.ToString(timeFormat) + ' ' + text : text);
+            logger.Write(AddTimestamp ? DateTime.Now.ToString(TimeFormat) + ' ' + text : text);
 
-            // Check for errors
-            if (logger.Failed)
-            {
-                Error(logger.LastError);
-            }
+            if (logger.Failed) Error(logger.LastError);
         }
 
-        // Error indication
         private static void Error(string err)
         {
             if (WriteFailed != null)
                 WriteFailed(null, new DataEventArgs<string>(err));
         }
 
-        // Data structure management
+        /// <summary>Registers a server connection for logging, creating its log file.</summary>
         public static void AddLoggable(Server network)
         {
-            // Add value to Network key to hold the different loggables
             LogFiles.Add(network.Url, new Dictionary<string, LoggedItem>());
-            // Add the network log
             LogFiles[network.Url].Add('!' + network.Url, new LoggedItem('!' + network.Url, network.Url));
         }
 
+        /// <summary>Registers a channel for logging, creating its log file.</summary>
         public static void AddLoggable(Channel chan)
         {
-            // Add channel log to the structure
             LogFiles[chan.Server.Url].Add(chan.Name, new LoggedItem(chan.Name, chan.Server.Url));
         }
 
+        /// <summary>Registers a private-message session for logging, creating its log file.</summary>
         public static void AddLoggable(Server network, User person)
         {
-            // Add pmsg log to the data struture
-            LogFiles[network.Url].Add(person.Nick, new LoggedItem(person.Nick, network.Url)); 
+            LogFiles[network.Url].Add(person.Nick, new LoggedItem(person.Nick, network.Url));
         }
 
+        /// <summary>Closes and removes all log files for a server and its channels.</summary>
         public static void RemoveLoggable(Server network)
         {
             if (!NetworkExists(network)) return;
 
-            foreach (LoggedItem log in LogFiles[network.Url].Values)
-            {
-                log.Close();
-            }
+            foreach (LoggedItem log in LogFiles[network.Url].Values) log.Close();
 
             LogFiles[network.Url].Clear();
             LogFiles.Remove(network.Url);
         }
 
+        /// <summary>Closes and removes the log file for a channel.</summary>
         public static void RemoveLoggable(Channel chan)
         {
             if (!ChannelExists(chan)) return;
@@ -101,6 +93,7 @@
             LogFiles[chan.Server.Url].Remove(chan.Name);
         }
 
+        /// <summary>Closes and removes the log file for a private-message session.</summary>
         public static void RemoveLoggable(Server network, User person)
         {
             if (!PersonExists(network, person)) return;
@@ -109,14 +102,12 @@
             LogFiles[network.Url].Remove(person.Nick);
         }
 
+        /// <summary>Closes and removes all log files.</summary>
         public static void RemoveAllLoggables()
         {
-            foreach(Dictionary<string, LoggedItem> innerDict in LogFiles.Values)
+            foreach (var innerDict in LogFiles.Values)
             {
-                foreach (LoggedItem li in innerDict.Values)
-                {
-                    li.Close();
-                }
+                foreach (LoggedItem li in innerDict.Values) li.Close();
 
                 innerDict.Clear();
             }
@@ -131,12 +122,12 @@
 
         private static bool ChannelExists(Channel chan)
         {
-            return (NetworkExists(chan.Server) && LogFiles[chan.Server.Url].ContainsKey(chan.Name));
+            return NetworkExists(chan.Server) && LogFiles[chan.Server.Url].ContainsKey(chan.Name);
         }
 
         private static bool PersonExists(Server network, User person)
         {
-            return (NetworkExists(network) && LogFiles[network.Url].ContainsKey(person.Nick));
+            return NetworkExists(network) && LogFiles[network.Url].ContainsKey(person.Nick);
         }
     }
 }
