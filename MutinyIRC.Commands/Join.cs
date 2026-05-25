bf095b75706972b91927193d2c6217a5a7afd866
Ortzinator <ortzinator@gmail.com>
2026-05-24 22:56:49 -0400

    [MutinyIRC] Add key overload to Join command

--- MutinyIRC.Commands/Join.cs
+++ MutinyIRC.Commands/Join.cs
@@ -32,5 +32,29 @@
         {
             Execute(context.Server, channelToJoin);
         }
+
+        /// <summary>
+        /// Joins you to the specified channel using the given key.
+        /// </summary>
+        public void Execute(Channel context, ChannelInfo channelToJoin, string key)
+        {
+            Execute(context.Server, channelToJoin, key);
+        }
+
+        /// <summary>
+        /// Joins you to the specified channel using the given key.
+        /// </summary>
+        public void Execute(Server context, ChannelInfo channelToJoin, string key)
+        {
+            context.JoinChannel(channelToJoin.Name, key);
+        }
+
+        /// <summary>
+        /// Joins you to the specified channel using the given key.
+        /// </summary>
+        public void Execute(PrivateMessageSession context, ChannelInfo channelToJoin, string key)
+        {
+            Execute(context.Server, channelToJoin, key);
+        }
     }
 }
